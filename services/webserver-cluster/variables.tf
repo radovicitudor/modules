e7b58c466c84658bd62835cee5d63012f73109b0
tudor.radovici <tudor.radovici@192-168-0-105.rdsnet.ro>
2021-02-09 00:51:07 +0200

    moded modules to separate repo

---
+++ services/webserver-cluster/variables.tf
@@ -0,0 +1,63 @@
+variable "server_port" {
+  description = "The port that will be used for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+variable "s3_bucket_name" {
+  description = "The S3 Bucket used for tfstate"
+  type        = string
+  default     = "terraform-tradov22"
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket used for the database's remote state storage"
+  type        = string
+}
+
+variable "db_remote_state_key" {
+  description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  type        = string
+}
+
+variable "alb_name" {
+  description = "The name of the ALB"
+  type        = string
+  default     = "terraform-asg-example"
+}
+
+variable "instance_security_group_name" {
+  description = "The name of the security group for the EC2 Instances"
+  type        = string
+  default     = "terraform-example-instance"
+}
+
+variable "alb_security_group_name" {
+  description = "The name of the security group for the ALB"
+  type        = string
+  default     = "terraform-example-alb"
+}
+
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 Instance to run"
+  type        = string
+}
+
+variable "min_size" {
+  description = "The minimum number of EC2 instances in the ASG"
+  type        = number
+}
+
+variable "max_size" {
+  description = "THe Maximum number of EC2 instances in the ASG"
+}
+
+/*variable "enable_autoscaling"{
+  description = "If set to true, enable autoscaling"
+  type = bool
+}*/
